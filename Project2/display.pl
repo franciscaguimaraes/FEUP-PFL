ba9ba38a352cea6d2e8a9e089f8eeb71f1597c36
brunabrasil <up202007191@g.uporto.pt>
2022-12-14 11:45:41 +0000

    added board and menu

--- Project2/display.pl
+++ Project2/display.pl
@@ -1,2 +1,71 @@
-player_symbol(p_yellow, 'Y').
-player_symbol(p_red, 'R').
+ initial(1, [
+  [ 1, 0, 1, 0, 1,-1],
+  [-1, 1,-1, 1,-1, 1],
+  [ 1,-1, 1,-1, 1,-1],
+  [-1, 1,-1, 1,-1, 1],
+  [ 1,-1, 1,-1, 1,-1],
+  [-1, 1,-1, 1,-1, 1]
+]).
+
+print_cell(cell(_, _, emptyCell)) :-
+  write('  ').
+
+code(0, 32).   % ascii code for space
+code(1, 215).  % × - Player 1
+code(-1, 216). % Ø - Player 2
+
+player_piece('Player 1', 1).
+player_piece('Player 2', -1).
+
+% Codes for board rows
+row(0, 'A').
+row(1, 'B').
+row(2, 'C').
+row(3, 'D').
+row(4, 'E').
+row(5, 'F').
+row(6, 'G').
+row(7, 'H').
+row(8, 'I').
+row(9, 'J').
+
+print_separator(0):-
+  write('+\n').
+print_separator(X):-
+  write('+---'), X1 is X-1, print_separator(X1).
+
+print_header_numbers(Initial, Initial):-
+  write(' '), 
+  write(Initial),
+  write(' |\n').
+print_header_numbers(Initial, Final):-
+  write(' '), 
+  write(Initial), 
+  write(' |'), 
+  N1 is Initial + 1,
+  print_header_numbers(N1, Final).
+
+print_header(X):-
+  write('   |'),
+  print_header_numbers(1, X),
+  write('---'),
+  print_separator(X).
+
+
+print_matrix([L|T], N, X):-
+    row(N,R), write('\n '), write(R), write(' | '), % letra da linha
+    N1 is N + 1,
+    print_line(L), nl,
+    write('---'), print_separator(X),
+    print_matrix(T, N1, X).
+print_matrix(_, _, X).
+
+print_line([]).
+
+print_line([C|L]):-
+  code(C, P),put_code(P), write(' | '),
+  print_line(L).
+
+display_game([L|T], N, X):-
+  print_header(X),
+  print_matrix([L|T], N, X).
