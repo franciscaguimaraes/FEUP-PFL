085ee08a70bf806a598e1fb74b866a70d3da494d
brunabrasil <up202007191@g.uporto.pt>
2022-12-27 18:04:27 +0000

    added logic functions

--- Project2/logic.pl
+++ Project2/logic.pl
@@ -1,9 +1,51 @@
+:-use_module(library(lists)).
 % value_in_board(+Board, +X, +Y, -Value)
 % returns in Value the value [0,1,-1] at (X,Y) from Board
-value_in_board(Board, X, Y, Value):-
-    nth0(Y, Board, Row),
-    nth0(X, Row, Value).
 
+bla(0, Acc, Acc).
+bla(-1, Acc, Acc1):-
+    Acc1 is Acc-1.
+bla(1, Acc, Acc1):-
+    Acc1 is Acc+1.
+
+value_in_board(Board, X, Y, Value):-
+    nth0(X, Board, Row),
+    nth0(Y, Row, Value).
+
+check_possible(Board, X, Y, Count):-
+    count_adjacentA(Board, X, Y, Value),
+    bla(Value, Count, Count1),
+    count_adjacentB(Board, X, Y, Value1),
+    bla(Value1, Count1, Count2),
+    count_adjacentL(Board, X, Y, Value2),
+    bla(Value2, Count2, Count3),
+    count_adjacentR(Board, X, Y, Value3),
+    bla(Value3, Count3, Count4),
+    Count4 =:= 0,
+    write('well well').
+
+check_possible(Board, X, Y, Count):-
+    write('Position not possible. Choose again!').
+
+% above
+count_adjacentA(Board, X, Y, Value) :-
+    X1 is X - 1,
+    value_in_board(Board, X1, Y, Value).
+
+% below
+count_adjacentB(Board, X, Y, Value) :-
+    X1 is X + 1,
+    value_in_board(Board, X1, Y, Value).
+
+% left
+count_adjacentL(Board, X, Y, Value) :-
+    Y1 is Y - 1,
+    value_in_board(Board, X, Y1, Value).
+
+% Right 
+count_adjacentR(Board, X, Y,  Value) :-
+    Y1 is Y + 1,
+    value_in_board(Board, X, Y1, Value).
 
 
 % replace_index(+I, +L, +E, -K)
@@ -31,9 +73,4 @@ move(GameState, X-Y-Direction, NewGameState):-
     replace(Board1, X1, Y1, Code, NewGameState).
 
 
-% count(+Num,+List, -X)
-% Returns in X the amount of Num in List
-count(_, [], 0).
-count(Num, [H|T], X) :- Num \= H, count(Num, T, X).
-count(Num, [H|T], X) :- Num = H, count(Num, T, X1), X is X1 + 1.
 
