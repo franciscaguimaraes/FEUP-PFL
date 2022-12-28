0fe441162c36deed6f795a6eaf7d1578cc61be7f
brunabrasil <up202007191@g.uporto.pt>
2022-12-28 14:26:34 +0000

    more of logic

--- Project2/display.pl
+++ Project2/display.pl
@@ -8,9 +8,6 @@ initial(1, [
 ]).
 
 
-print_cell(cell(_, _, emptyCell)) :-
-  write('  ').
-
 % size_of_board(+Board, -X)
 % returns in X the size of the Square Board. Does not accept Rectangular Boards
 size_of_board(Board, X):-
@@ -56,7 +53,7 @@ print_header_numbers(Initial, Final):-
 
 print_header(X):-
   write('   |'),
-  print_header_numbers(1, X),
+  print_header_numbers(0, X),
   write('---'),
   print_separator(X).
 
@@ -75,7 +72,7 @@ print_line([C|L]):-
   print_line(L).
 
 display_game(Board):-
-  nl,
+  clear,
   size_of_board(Board, X),
   print_header(X),
   print_matrix(Board, 0, X).
--- Project2/logic.pl
+++ Project2/logic.pl
@@ -1,49 +1,59 @@
-:-use_module(library(lists)).
-% value_in_board(+Board, +X, +Y, -Value)
-% returns in Value the value [0,1,-1] at (X,Y) from Board
 
-bla(0, Acc, Acc).
-bla(-1, Acc, Acc1):-
+counter(0, Acc, Acc).
+counter(-1, Acc, Acc1):-
     Acc1 is Acc-1.
-bla(1, Acc, Acc1):-
+counter(1, Acc, Acc1):-
     Acc1 is Acc+1.
 
+% value_in_board(+Board, +X, +Y, -Value)
+% returns in Value the value [0,1,-1] at (X,Y) from Board
 value_in_board(Board, X, Y, Value):-
     nth0(X, Board, Row),
     nth0(Y, Row, Value).
 
-check_possible(Board, X, Y, Count):-
-    count_adjacentA(Board, X, Y, Value),
-    bla(Value, Count, Count1),
-    count_adjacentB(Board, X, Y, Value1),
-    bla(Value1, Count1, Count2),
-    count_adjacentL(Board, X, Y, Value2),
-    bla(Value2, Count2, Count3),
-    count_adjacentR(Board, X, Y, Value3),
-    bla(Value3, Count3, Count4),
-    Count4 =:= 0,
-    write('well well').
+% check_possible(+Board, +X, +Y, +Count)
+% checks if chosen position to place a piece is free and if it is surrounded by the same number of pieces
+check_possible(Board, X, Y, _, _):-
+    value_in_board(Board, X, Y, Value),
+    Value \= 0,
+    write('This position is already occupied! Choose again!').
 
-check_possible(Board, X, Y, Count):-
+check_possible(Board, X, Y, Count, Player):-
+    value_north(Board, X, Y, Value),
+    counter(Value, Count, Count1),
+    value_south(Board, X, Y, Value1),
+    counter(Value1, Count1, Count2),
+    value_left(Board, X, Y, Value2),
+    counter(Value2, Count2, Count3),
+    value_right(Board, X, Y, Value3),
+    counter(Value3, Count3, Count4),
+    Count4 =:= 0,
+    replace(Board, X, Y, Player, BoardResult).
+
+check_possible(_, _, _, _, _):-
     write('Position not possible. Choose again!').
 
-% above
-count_adjacentA(Board, X, Y, Value) :-
+% value_north(+Board, +X, +Y, -Value)
+% returns the value [1,-1,0] of the piece above the piece chosen
+value_north(Board, X, Y, Value) :-
     X1 is X - 1,
     value_in_board(Board, X1, Y, Value).
 
-% below
-count_adjacentB(Board, X, Y, Value) :-
+% value_south(+Board, +X, +Y, -Value)
+% returns the value [1,-1,0] of the piece below the piece chosen
+value_south(Board, X, Y, Value) :-
     X1 is X + 1,
     value_in_board(Board, X1, Y, Value).
 
-% left
-count_adjacentL(Board, X, Y, Value) :-
+% value_left(+Board, +X, +Y, -Value)
+% returns the value [1,-1,0] of the piece on the left the piece chosen
+value_left(Board, X, Y, Value) :-
     Y1 is Y - 1,
     value_in_board(Board, X, Y1, Value).
 
-% Right 
-count_adjacentR(Board, X, Y,  Value) :-
+% value_right(+Board, +X, +Y, -Value)
+% returns the value [1,-1,0] of the piece on the right the piece chosen
+value_right(Board, X, Y,  Value) :-
     Y1 is Y + 1,
     value_in_board(Board, X, Y1, Value).
 
@@ -56,21 +66,13 @@ replace_index(I, L, E, K) :-
 
 % replace(+Board, +X, +Y, +Value, -BoardResult)
 % replaces a value in the board
-replace(Board, X, Y, Value, BoardResult):-
+replace(Board, X, Y, Player, BoardResult):-
 %usar substitute(+X, +Xlist, +Y, ?Ylist)
     nth0(Y, Board, Row),
-    replace_index(X, Row, Value, NewRow),
-    replace_index(Y, Board, NewRow, BoardResult).
+    replace_index(X, Row, Player, NewRow),
+    replace_index(Y, Board, NewRow, BoardResult),
+    display_game(BoardResult).
 
-
-% MUDAR ISTOOO
-% move(+GameState, +X-Y-Direction, -NewGameState)
-%  performs the change in the board, replaces current piece with 0 and enemy piece with player code
-move(GameState, X-Y-Direction, NewGameState):-
-    value_in_board(GameState, X, Y, Code),
-    replace(GameState, X, Y, 0, Board1),
-    direction(X-Y, Direction, X1, Y1),
-    replace(Board1, X1, Y1, Code, NewGameState).
 
 
 
