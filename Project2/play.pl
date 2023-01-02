d5263c8447d5c381cc828d3a91e6ca98a5c5d874
Francisca Guimarães <franciscahguimaraes@gmail.com>
2023-01-02 11:25:39 +0000

    play comments

--- Project2/play.pl
+++ Project2/play.pl
@@ -1,33 +1,39 @@
+% given type of players, gives mode to start playing
 get_play_mode('Human', 'Human', 1).
 get_play_mode('Human', 'Computer', 2).
 get_play_mode('Computer', 'Human', 3).
 get_play_mode('Computer', 'Computer', 4).
 
+
+% given play mode, starts game accordingly
 play_mode(1, GameState, _) :- play_pp(GameState, 1). %Player vs Player
 play_mode(2, GameState, Difficulty) :- play_pc(GameState, 1, Difficulty). %Player vs Computer
 play_mode(3, GameState, Difficulty) :- play_cp(GameState, 1, Difficulty). %Computer vs Player 
 play_mode(4, GameState, Difficulty) :- play_cc(GameState, 1, Difficulty). %Computer vs Computer
 
-% alternatePlayer(+Player, -OtherPlayer)
+
+% alternates players between games
 alternatePlayer(1, 2).
 alternatePlayer(2, 1).
 
-is_human_pc(1).
-is_human_cp(2).
 
-play :-
-  clear,
-  main_menu, !.
+% checks if, given play mode, player is human
+is_human_pc(1). % PlayervsComputer - player 1st
+is_human_cp(2). % ComputervsPlayer - player 2nd
 
+
+% start_game(+Number, +TypePlayer1, +TypePlayer2, +Difficulty)
+% starts game given boardSize, play mode and difficulty 
 start_game(Number, TypePlayer1, TypePlayer2, Difficulty):-
   clear, 
-  board(Number, Size),
   initial_state(Size, GameState),
   display_board(GameState),
   get_play_mode(TypePlayer1, TypePlayer2, Mode),
   play_mode(Mode, GameState, Difficulty).
 
+
 % play_pp(+GameState, +Player)
+% starts Player vs Player game loop
 play_pp(GameState, Player) :-
   game_over(GameState), nl,
   format( '| Player ~d - ' , [Player]), write_player(Player), write( ' - Lost!'), nl,
@@ -35,7 +41,6 @@ play_pp(GameState, Player) :-
   format( '| Player ~d - ' , [NewPlayer]), write_player(NewPlayer), write( ' - Won!'), nl,
   sleep(5),
   main_menu.
-
 play_pp(GameState, Player):-
   choose_move_human(GameState, Player, Row, Col),
   replace(GameState, Row, Col, Player, NewGameState),
@@ -43,37 +48,39 @@ play_pp(GameState, Player):-
   alternatePlayer(Player, NewPlayer),
   play_pp(NewGameState, NewPlayer).
 
+
 % choose_move_human(+GameState, +Player, -Row, -Col)
+% predicate that asks user for input 
 choose_move_human(GameState, Player, Row, Col) :- 
   size_of_board(GameState, Size),
   format( '~n| Player ~d - ' , [Player]), write_player(Player), write( ' - make a move! \n\n'),
   read_inputs(Size, Row, Col), 
   check_move(GameState, Player, Row, Col).
 
-% check_move(+ListOfMoves,+Row,+Col)
-% verifica se um movimento está presenta na Lista de movimentos dada
+
+% check_move(+GameState, +Player, +Row, +Col)
+% verifies if row and col from user input is present in list of valid moves
 check_move(GameState, _, Row, Col) :-
   check_position(GameState, Row, Col).
 check_move(GameState, Player, _, _):-
   write('\n| Invalid position. Choose again!\n'),
   play_pp(GameState, Player).
 
-% ------------------------------------
 
+% play_pc(GameState, Player, Difficulty)
+% starts Player vs Computer game loop
 play_pc(GameState, 1, _) :-
   game_over(GameState), nl,
   write( '| Player - '), write_player(1), write( ' - Lost!'), nl,
   write( '| Computer - '), write_player(2), write( ' - Won!'), nl,
   sleep(5),!,
   main_menu.
-
 play_pc(GameState, 2, _) :-
   game_over(GameState), nl,
   write( '| Computer - '), write_player(2), write( ' - Lost!'), nl,
   write( '| Player - '), write_player(1), write( ' - Won!'), nl,
   sleep(5), !,
   main_menu.
-
 play_pc(GameState, Player, Difficulty) :-  
   is_human_pc(Player),
   choose_move_human(GameState, Player, Row, Col),
@@ -81,7 +88,6 @@ play_pc(GameState, Player, Difficulty) :-
   display_board(NewGameState),
   alternatePlayer(Player, NewPlayer),
   play_pc(NewGameState, NewPlayer, Difficulty).
-
 play_pc(GameState, Player , Difficulty) :-  
   choose_move_computer(GameState, Difficulty, Player, Row, Col),
   letter_number(Column, Col),
@@ -92,22 +98,21 @@ play_pc(GameState, Player , Difficulty) :-
   alternatePlayer(Player, NewPlayer),
   play_pc(NewGameState, NewPlayer, Difficulty).
 
-% ---------------------------------------------------
 
+% play_cp(GameState, Player, Difficulty)
+% starts Computer vs Player game loop
 play_cp(GameState, 2, _) :-
   game_over(GameState), nl,
   write( '| Player - '), write_player(1), write( ' - Lost!'), nl,
   write( '| Computer - '), write_player(2), write( ' - Won!'), nl,
   sleep(5),!,
   main_menu.
-
 play_cp(GameState, 1, _) :-
   game_over(GameState), nl,
   write( '| Computer - '), write_player(2), write( ' - Lost!'), nl,
   write( '| Player - '), write_player(1), write( ' - Won!'), nl,
   sleep(5), !,
   main_menu.
-
 play_cp(GameState, Player, Difficulty) :-  
   is_human_cp(Player),
   choose_move_human(GameState, Player, Row, Col),
@@ -115,7 +120,6 @@ play_cp(GameState, Player, Difficulty) :-
   display_board(NewGameState),
   alternatePlayer(Player, NewPlayer),
   play_cp(NewGameState, NewPlayer, Difficulty).
-
 play_cp(GameState, Player , Difficulty) :-  
   choose_move_computer(GameState, Difficulty, Player, Row, Col),
   letter_number(Column, Col),
@@ -126,8 +130,9 @@ play_cp(GameState, Player , Difficulty) :-
   alternatePlayer(Player, NewPlayer),
   play_cp(NewGameState, NewPlayer, Difficulty).
 
-% --------------------
 
+% play_cc(GameState, Player, Difficulty) 
+% starts Computer vs Computer game loop
 play_cc(GameState, Player, _) :-
   game_over(GameState), nl,
   format( '| Computer ~d - ' , [Player]), write_player(Player), write( ' - Lost!'), nl,
@@ -135,7 +140,6 @@ play_cc(GameState, Player, _) :-
   format( '| Computer ~d - ' , [NewPlayer]), write_player(NewPlayer), write( ' - Won!'), nl,
   sleep(5),
   main_menu. 
-
 play_cc(GameState, Player, Difficulty) :-  
   choose_move_computer(GameState, Difficulty, Player, Row, Col),
   letter_number(Column, Col),
@@ -146,7 +150,9 @@ play_cc(GameState, Player, Difficulty) :-
   alternatePlayer(Player, NewPlayer),
   play_cc(NewGameState, NewPlayer, Difficulty).
 
+
 % game_over(+GameState)
+% checks if list of valid moves is null, meaning no more plays to make and game over
 game_over(GameState):-
   valid_moves(GameState, ListOfMoves),
   length(ListOfMoves, 0).
