b300324bb42886d9e454faff7c44da399ef5c99e
Francisca Guimarães <franciscahguimaraes@gmail.com>
2023-01-02 11:36:17 +0000

    computer comments

--- Project2/computer.pl
+++ Project2/computer.pl
@@ -1,16 +1,19 @@
-% choose_move(+GameState, +Player, +Difficulty, -Row, -Col)
-choose_move_computer(Board, 1, _, Row, Col):-
+% choose_move_computer(+GameState, +Difficulty, +Player -Row, -Col)
+% predicate that, according to difficulty, makes a random or a greedy move, calculating possible moves
+choose_move_computer(Board, 'Random', _, Row, Col):-
     valid_moves(Board, ListOfMoves),
     length(ListOfMoves, L),
     random(0, L, Position),
     nth0(Position, ListOfMoves, Row-Col).
-
-choose_move_computer(GameState, 2, Player, Row, Col):-
+choose_move_computer(GameState, 'Greedy', Player, Row, Col):-
     valid_moves(GameState, ListOfMoves),
     counter_of_moves(ListOfMoves, GameState, Player, Result),
-    samsort(Result, OrderedSolution), % cant have random bc when availabeMoves is only 1 for example and random picks number 3, crashes
+    samsort(Result, OrderedSolution), 
     nth0(0, OrderedSolution, _-Row-Col).
 
+
+% counter_of_moves(+ListOfMoves, +GameState, +Player, -Result)
+% predicate that counts the number of moves the opposite player can take for each move player makes. Result is the move which results in less moves for the opposite player
 counter_of_moves([], _, _, []).
 counter_of_moves([ X-Y | L], GameState, Player, Result):-
     replace(GameState, X, Y, Player, NewGameState), 
@@ -18,6 +21,8 @@ counter_of_moves([ X-Y | L], GameState, Player, Result):-
     length(Options, Plays),
     counter_of_moves(L, GameState, Player, NewResult),
     append(NewResult, [Plays-X-Y], Result).
+
+
 
 sort_by_plays(List, Sorted) :- 
     predsort(compare_plays, List, Sorted).
