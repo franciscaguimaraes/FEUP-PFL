e7551c4e80a670c17616c759976694e893cce409
Francisca Guimarães <franciscahguimaraes@gmail.com>
2023-01-02 10:41:02 +0000

    logic comments

--- Project2/logic.pl
+++ Project2/logic.pl
@@ -1,3 +1,8 @@
+
+% added -1??
+
+% counter(+Number, +Acc, -Acc1)
+% Counter used to count pieces around position to be checked.
 counter(0, Acc, Acc).
 counter(-1, Acc, Acc).
 counter(2, Acc, Acc1):-
@@ -5,8 +10,9 @@ counter(2, Acc, Acc1):-
 counter(1, Acc, Acc1):-
     Acc1 is Acc+1.
 
+
 % value_in_board(+Board, +X, +Y, -Value)
-% returns in Value the value [0,1,2] at (X,Y) from Board 
+% returns in Value the value [-1,0,1,2] at (X,Y) from Board.
 value_in_board(_, -1, _, 0).
 value_in_board(_, _, -1, 0).
 value_in_board(Board, X, Y, 0):-
@@ -16,9 +22,9 @@ value_in_board(Board, X, Y, Value):-
     nth0(X, Board, Row),
     nth0(Y, Row, Value).
 
-% check_place(+Board, +X, +Y, +Count)
-% checks if chosen position to place a piece is free and if it is surrounded by the same number of pieces
 
+% can_place(+Board, +X, +Y, -Result)
+% checks if chosen position to place a piece is free and if it is surrounded by the same number of pieces
 can_place(Board, X, Y, Result):-
     value_north(Board, X, Y, Value),
     counter(Value, 0, Count1),
@@ -29,47 +35,55 @@ can_place(Board, X, Y, Result):-
     value_right(Board, X, Y, Value3),
     counter(Value3, Count3, Result).
 
+
 % value_north(+Board, +X, +Y, -Value)
-% returns the value [1,2,0] of the piece above the piece chosen
+% returns the value [-1,1,2,0] of the piece above the piece chosen
 value_north(Board, X, Y, Value) :-
     X1 is X - 1,
     value_in_board(Board, X1, Y, Value).
 
+
 % value_south(+Board, +X, +Y, -Value)
-% returns the value [1,2,0] of the piece below the piece chosen
+% returns the value [-1,1,2,0] of the piece below the piece chosen
 value_south(Board, X, Y, Value) :-
     X1 is X + 1,
     value_in_board(Board, X1, Y, Value).
 
+
 % value_left(+Board, +X, +Y, -Value)
-% returns the value [1,2,0] of the piece on the left the piece chosen
+% returns the value [-1,1,2,0] of the piece on the left the piece chosen
 value_left(Board, X, Y, Value) :-
     Y1 is Y - 1,
     value_in_board(Board, X, Y1, Value).
 
+
 % value_right(+Board, +X, +Y, -Value)
-% returns the value [1,2,0] of the piece on the right the piece chosen
+% returns the value [-1,1,2,0] of the piece on the right the piece chosen
 value_right(Board, X, Y,  Value) :-
     Y1 is Y + 1,
     value_in_board(Board, X, Y1, Value).
 
 
-% replace_index(+I, +L, +E, -K)
-% replaces Element E in List L at Index I, Resulting in List K
-replace_index(I, L, E, K) :-
-    nth0(I, L, _, R),
-    nth0(I, K, E, R).
+% replace_index(+Index, +List, +Element, -RList)
+% replaces Element in List at Index, Resulting in RList
+replace_index(Index, List, Element, RList) :-
+    nth0(Index, List, _, R),
+    nth0(Index, RList, Element, R).
+
 
 % replace(+Board, +X, +Y, +PLayer, -BoardResult)
-% replaces a value in the board
+% replaces a value in the board resulting in BoardResult
 replace(Board, X, Y, Player, BoardResult):-
     nth0(X, Board, Row),
     replace_index(Y, Row, Player, NewRow),
     replace_index(X, Board, NewRow, BoardResult).
 
+
 % valid_moves(+GameState, -ListOfMoves)
+% returns a ListOfMoves containing all possible moves for GameState
 valid_moves(GameState, ListOfMoves):-
     findall(X-Y, check_position(GameState, X, Y), ListOfMoves).
+
 
 % check_position(+Board, +X, +Y)
 % checks if it is valid put a piece in position (X,Y)
